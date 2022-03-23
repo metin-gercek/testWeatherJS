1780840821a05183efeedb8280b32e74c19a3ba4
metin-gercek <metin.gercek@edu.bc.fi>
2022-03-23 21:08:04 +0200

    add scripts

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-var input = document.querySelector(".input_text");
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
-console.log(input);
+
+button.addEventListener('click', function(name){
+    
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  tempValue = tempValue-273.15;
+  tempValue = tempValue.toFixed(0);
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
